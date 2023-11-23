7750cda163cca659fd030c038fd663012eee8b94
Maria <mi.kovalenko@mail.ru>
2023-11-24 00:34:34 +0400

    config

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Set the base URL for your project
+  base: '/test_smart/', // Replace <repository-name> with your GitHub repository name
+});
